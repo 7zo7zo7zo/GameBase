e0ac285e3f33e3bf73fe72af510796fcd16baeb1
7zo7zo7zo <77756772+7zo7zo7zo@users.noreply.github.com>
2021-07-25 17:30:35 -0700

    Update AnimatedImageEntity.java

--- AnimatedImageEntity.java
+++ AnimatedImageEntity.java
@@ -1,35 +1,26 @@
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-import java.util.ArrayList;
 
 public class AnimatedImageEntity extends ImageEntity {
 
-    protected SpriteSheet sheet;
-
     protected int currentFrame = 0;
 
-    protected ArrayList<BufferedImage> frames;
+    protected BufferedImage[] frames;
 
-    public AnimatedImageEntity(Game game, float x, float y, int width, int height, int offset, String path,
-                               int totalFrames) {
+    public AnimatedImageEntity(Game game, float x, float y, int width, int height, int offset, String path) {
         super(game, x, y, width, height, offset, path);
 
-        sheet = new SpriteSheet(image);
 
-        frames = new ArrayList<BufferedImage>();
-
-        for (int i = 0; i < totalFrames; i++) {
-            frames.add(sheet.crop(width * i, 0, width, height));
-        }
+        frames = ImageLoader.loadImagesFromSpriteSheet(path, width, height);
     }
 
     public void render(Graphics2D g2) {
-        image = frames.get(currentFrame);
+        image = frames[currentFrame];
         super.render(g2);
     }
 
     public void cycle() {
-        if (currentFrame + 1 >= frames.size()) {
+        if (currentFrame + 1 >= frames.length) {
             currentFrame = 0;
         } else {
             currentFrame++;
