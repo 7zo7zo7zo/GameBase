f40dfaaf8153cd14189bb2819f596820f29cbe1a
7zo7zo7zo <77756772+7zo7zo7zo@users.noreply.github.com>
2022-05-01 22:43:51 -0700

    Add files via upload

---
+++ Assets.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+
+public class Assets {
+
+    private Game game;
+    private Input input;
+
+    public Assets(Game game) {
+        this.game = game;
+        input = game.getInput();
+    }
+
+    public void tick() {
+
+    }
+
+    public void render(Graphics2D g) {
+
+    }
+}
